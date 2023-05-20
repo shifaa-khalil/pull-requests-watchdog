18aa181b0b1e04162b0205f2588af4dcc596e356
ShifaaKhalil98 <shifaakhalil50@gmail.com>
2023-05-20 22:19:15 +0300

    created an API for retrieving PR where review is not requested

--- app/Http/Controllers/PRController.php
+++ app/Http/Controllers/PRController.php
@@ -6,31 +6,39 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Collection;
 use DateTime;
+use DateInterval;
 
 class PRController extends Controller
 {
     public function pullRequests()
     {
+        $fourteenDaysAgo = (new DateTime())->sub(new DateInterval('P14D'))->format('Y-m-d');
+
         $response = Http::withHeaders([
-            'Accept' => 'application/vnd.github.v3+json',
-            'Authorization' => 'Bearer ' . config('app.github_token'),
-        ])->get('https://api.github.com/repos/woocommerce/woocommerce/pulls');
+        'Accept' => 'application/vnd.github.v3+json',
+        'Authorization' => 'Bearer ' . config('app.github_token'),
+        ])->get('https://api.github.com/search/issues?q=repo:woocommerce/woocommerce+is:pr+created:<'.$fourteenDaysAgo);
+
+        // $response = Http::withHeaders([
+        //     'Accept' => 'application/vnd.github.v3+json',
+        //     'Authorization' => 'Bearer ' . config('app.github_token'),
+        // ])->get('https://api.github.com/repos/woocommerce/woocommerce/pulls');
 
         $pullRequests = collect($response->json());
 
-        $today = new DateTime();
-        // $today = $todayObj->format('Y-m-d');
+        // $today = new DateTime();
+        // // $today = $todayObj->format('Y-m-d');
 
-        $array = [];
+        // $array = [];
 
-        $pullRequests->map(function ($PR) use (&$array, $today){
-            $date = new DateTime($PR['created_at']);
-            // $date = $date->format('Y-m-d');
-            $interval = $today->diff($date);
-            $diff = $interval->days;
-            if($diff>4) $array[] = $PR;
-        });
-        return $array;
+        // $pullRequests->map(function ($PR) use (&$array, $today){
+        //     $date = new DateTime($PR['created_at']);
+        //     // $date = $date->format('Y-m-d');
+        //     $interval = $today->diff($date);
+        //     $diff = $interval->days;
+        //     if($diff>4) $array[] = $PR;
+        // });
+        return $pullRequests;
 
         // Process and filter the pull request data based on your requirements
         // ...
@@ -45,10 +53,31 @@ class PRController extends Controller
             'Accept' => 'application/vnd.github.v3+json',
             'Authorization' => 'Bearer ' . config('app.github_token'),
         ])->get('https://api.github.com/repos/woocommerce/woocommerce/pulls?q=is%3Apr+is%3Aopen+review%3Arequired');        
-
         $pullRequests = collect($response->json());
 
         return $pullRequests;
+    }
+
+    public function statusSuccessPR()
+    {
+        $response = Http::withHeaders([
+            'Accept' => 'application/vnd.github.v3+json',
+            'Authorization' => 'Bearer ' . config('app.github_token'),
+        ])->get('https://api.github.com/repos/woocommerce/woocommerce/pulls?q=is%3Apr+is%3Aopen+status%3Asuccess');        
+        
+        $pullRequests = collect($response->json());
+
+        return $pullRequests;
+    }
+    
+    public function noReviewPR()
+    {
+        $response = Http::withHeaders([
+            'Accept' => 'application/vnd.github.v3+json',
+            'Authorization' => 'Bearer ' . config('app.github_token'),
+        ])->get('https://api.github.com/repos/woocommerce/woocommerce/pulls?q=is%3Apr+is%3Aopen+no%3Areview-requested');
+
+        return $response->json();
     }
 }
 
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\PRController;
 
 Route::get('/pull-requests', [PRController::class, 'pullRequests']);
 Route::get('/review-required-pr', [PRController::class, 'reviewRequiredPR']);
+Route::get('/status-success-pr', [PRController::class, 'statusSuccessPR']);
+Route::get('/no-review-pr', [PRController::class, 'noReviewPR']);
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
